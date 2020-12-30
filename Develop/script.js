84dcfcf0971700fa2f1a808c7ff4353a02ebe2d9
Snow Day <derek.miller826@gmail.com>
2020-12-30 12:30:32 -0600

    added first version for lowercase, uppercase, numbers, special characters

--- Develop/script.js
+++ Develop/script.js
@@ -14,29 +14,43 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  // ask player if they'd like to fight or run
+
+  // ask the user for the number of characters they want for the password
   var promptNumberOfChar = window.prompt('Enter number of characters for password. Between 8 and 128.');
   while (promptNumberOfChar < 8 || promptNumberOfChar > 128 ) {
     window.alert('You must enter a number between 8 and 128!');
-    var promptNumberOfChar = window.prompt('Enter number of characters for password. Between 8 and 128.');
+    promptNumberOfChar = window.prompt('Enter number of characters for password. Between 8 and 128.');
   }
 
+  //ask the user for the types of character they want for 
   var promptCharSet = window.prompt('Enter selection for password character set. Enter lowercase, uppercase, numbers, special characters.');
+  promptCharSet = promptCharSet.toLowerCase;
+  while (promptCharSet != 'lowercase' || promptCharSet != 'uppercase' || promptCharSet != 'numbers' || promptCharSet != 'special characters') {
+    window.alert('You must enter at least one of the following: lowercase, uppercase, numbers, special characters');
+    promptCharSet = window.prompt('Enter selection for password character set. Enter lowercase, uppercase, numbers, special characters.');
+  }
+  // initialize charSetLower, charSetUpper, charSetNumber, charSetSpecial
 
-  //THEN I choose lowercase, uppercase, numeric, and/or special characters
-  var userTurn = prompt('Enter r, p, or s to play!');
-        userTurn = userTurn.toLowerCase();
+  charSetLower = "";
+  charSetUpper = "";
+  charSetNumber = "";
+  charSetSpecial = "";
 
-        // Create the game logic to run if the user chooses a valid game choice
-        if (userTurn === 'r' || userTurn === 'p' || userTurn === 's') {
+  if (promptCharSet === 'lowercase') {
+    charSetLower = "abcdefghijklmnopqrstuvwxyz"
+  } if (promptCharSet === 'uppercase') {
+    charSetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  } if (promptCharSet === 'numbers'){
+    charSetNumber = "0123456789"
+  } if (promptCharSet === 'special characters'){
+    charSetSpecial = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  }
 
   var password = generatePassword(promptNumberOfChar, charSetLower, charSetUpper, charSetNumber, charSetSpecial);
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 };
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
